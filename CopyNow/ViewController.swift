c032fe02659a1e8ddc1d03a1c270680512229686
yjham2002 <yjham2002@gmail.com>
2016-12-24 03:14:18 +0900

    Initial Commit

---
+++ CopyNow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CopyNow
+//
+//  Created by 함의진 on 2016. 12. 24..
+//  Copyright © 2016년 함의진. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
