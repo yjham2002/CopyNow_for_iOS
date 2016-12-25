464593b1f920b7fb2790f6e8cb81f39b5c8af27c
yjham2002 <yjham2002@gmail.com>
2016-12-26 07:00:06 +0900

    TableView added

--- CopyNow/ViewController.swift
+++ CopyNow/ViewController.swift
@@ -8,10 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let dates = ["2014","2015","2016"]
+    let cnts = ["swift","iOS","Testing"]
+    
+    @IBOutlet weak var myTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        myTable.delegate = self;
+        myTable.dataSource = self;
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,12 +28,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func onClick(_ sender: Any) {
-        let alert = UIAlertController(title: "Alert", message: "Choose it!", preferredStyle: UIAlertControllerStyle.actionSheet)
-        let alertOk = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil)
-        alert.addAction(alertOk)
-        
-        present(alert, animated: true, completion: nil)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection: Int) -> Int {
+        return dates.count
     }
-
+    
+    // 각 row 마다 데이터 세팅.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 첫 번째 인자로 등록한 identifier, cell은 as 키워드로 앞서 만든 custom cell class화 해준다.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath as IndexPath) as! listCell
+        
+        cell.dateLabel.text = dates[indexPath.row]
+        cell.contentLabel.text = cnts[indexPath.row]
+        
+        return cell
+    }
+    
 }
---
+++ CopyNow/listCell.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class listCell: UITableViewCell {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var delButton: UIButton!
+    
+}
