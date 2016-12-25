09d07dda298a644491713c2d0c44dd5d515c8bd8
yjham2002 <yjham2002@gmail.com>
2016-12-26 04:04:49 +0900

    initial commit

--- CopyNow/ViewController2.swift
+++ CopyNow/ViewController2.swift
@@ -8,24 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController2: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func onClick(_ sender: Any) {
-        let alert = UIAlertController(title: "Alert", message: "Choose it!", preferredStyle: UIAlertControllerStyle.actionSheet)
-        let alertOk = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil)
-        alert.addAction(alertOk)
         
-        present(alert, animated: true, completion: nil)
     }
-
+    
+    
 }
